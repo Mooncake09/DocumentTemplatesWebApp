a37088657427fdbeef807d4c2c5e270ecbaff15c
mooncake09 <tabak1805@gmail.com>
2022-10-05 19:27:20 +0300

    Add swagger

--- DocumentTemplatesWebApp/Program.cs
+++ DocumentTemplatesWebApp/Program.cs
@@ -1,5 +1,23 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo {
+    Version = "v1",
+    Title = "Documets generator API",
+    Description = "API for generating documents from a template"
+}));
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
